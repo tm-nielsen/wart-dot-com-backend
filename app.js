0f4f8ededced3631ba1841397ec6a3c2cf33c0c4
Taylor Nielsen <littleb88888888@gmail.com>
2023-09-16 00:19:35 -0600

    Tweaked app into being functional

--- app.js
+++ app.js
@@ -1,32 +1,49 @@
 const Express = require('express')
 const testData = require('./testData')
 
-const app = Express()
 
-app.get('/', (req, res) =>{
-    res.send("Hello World")
-})
+exports.createApp = (runArgument) => {
+    const app = Express()
 
-app.get('/active', (req, res) => {
-    var activePrompt = testData['active']
-    res.send(activePrompt)
-})
-
-app.get('/category', (req, res) => {
-    var categoryName = req.body
-    if (categoryName in testData)
-        res.send(JSON.stringify(testData[categoryName]))
-    else
-        res.send("invalid category")
-})
-
-app.post('/', (req, res) => {
-    res.send("recieved post request")
-})
-
-app.patch('', (req, res) => {
-    res.send("recieved patch request")
-})
+    app.use((req, res, next)=>{
+        let allowedOrigin = 'https://tm-nielsen.github.io/wart-dot-com/'
+        if (runArgument === 'debug')
+            allowedOrigin = 'http://localhost:3000'
+        res.setHeader('Access-Control-Allow-Origin', allowedOrigin);
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        next();
+    })
 
 
-exports.expressApp = app
+    app.get('/', (req, res) =>{
+        res.send("Hello World")
+    })
+
+    app.get('/active', (req, res) => {
+        var activePrompt = testData['active']
+        res.send(activePrompt)
+    })
+
+    app.get('/category/:categoryName', (req, res) => {
+        var categoryName = req.params.categoryName
+        if (categoryName in testData)
+            res.send(JSON.stringify(testData[categoryName]))
+        else
+            res.send("invalid category")
+    })
+
+    app.post('/', (req, res) => {
+        res.send("recieved post request")
+    })
+
+    app.patch('', (req, res) => {
+        res.send("recieved patch request")
+    })
+
+    app.delete('', (req, res) => {
+        res.send('recieved delete request')
+    })
+
+    return app
+}
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const { expressApp } = require('./app.js')
+const { createApp } = require('./app.js')
 
 const dotenv = require('dotenv')
 dotenv.config()
 
-
+const expressApp = createApp(process.argv[2])
 expressApp.listen(process.env.PORT)
 console.log('express app listening on ', process.env.PORT)
+console.log(process.argv[2])
