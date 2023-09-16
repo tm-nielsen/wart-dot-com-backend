7ce068f26741c245d66a74ee9560df47efd53c19
Taylor Nielsen <littleb88888888@gmail.com>
2023-09-15 23:04:38 -0600

    Sketched express app

---
+++ app.js
@@ -0,0 +1,32 @@
+import Express from'express'
+import testData from 'testData'
+
+const app = Express()
+
+app.get('/', (req, res) =>{
+    res.send("Hello World")
+})
+
+app.get('/active', (req, res) => {
+    var activePrompt = testData['active']
+    res.send(activePrompt)
+})
+
+app.get('/category', (req, res) => {
+    var categoryName = req.body
+    if (categoryName in testData)
+        req.send(JSON.stringify(testData[categoryName]))
+    else
+        req.send("invalid category")
+})
+
+app.post('/', (req, res) => {
+    res.send("recieved post request")
+})
+
+app.patch('', (req, res) => {
+    res.send("recieved patch request")
+})
+
+
+export default app
