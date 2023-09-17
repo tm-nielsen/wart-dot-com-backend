5226d48b2287d3ebab5230f52d45b5da9b889f48
Taylor Nielsen <littleb88888888@gmail.com>
2023-09-17 00:14:46 -0600

    Added authenticated method skeletons

--- app.js
+++ app.js
@@ -37,18 +37,64 @@ exports.createApp = (isDebugging = false) => {
         }, isDebugging)
     })
 
-    app.post('/', (req, res) => {
+    app.get('/authenticate/:password', (req, res) => {
+        const password = req.params.password
+        if (password === process.env.PASSWORD)
+            res.send(true)
+        else
+            res.send(false)
+    })
+
+
+    app.post('/suggest', (req, res) => {
         const {prompt} = req.body
         addPendingPrompt(prompt, isDebugging)
-        res.send('received post request')
+        res.send(`added prompt "${prompt}"`)
     })
 
-    app.patch('', (req, res) => {
-        res.send('received patch request')
+
+    const authenticateRequest = (req, res) => {
+        const {password} = req.body
+        if (password !== process.env.PASSWORD){
+            res.status(401)
+            res.send("unauthorized access")
+            return false
+        }
+        return true
+    }
+
+    app.post('insert', (req, res) => {
+        if (authenticateRequest(req, res)) {
+            const {prompt, category} = req.body
+            res.send(`inserted prompt ${prompt} into ${category}`)
+        }
     })
 
-    app.delete('', (req, res) => {
-        res.send('received delete request')
+    app.patch('/approve', (req, res) => {
+        if (authenticateRequest(req, res)) {
+            const {approvedPrompts} = req.body
+            res.send(`received approval request for ${approvedPrompts}`)
+        }
+    })
+
+    app.patch('/reject', (req, res) => {
+        if (authenticateRequest(req, res)) {
+            const {rejectedPrompts} = req.body
+            res.send(`received rejection request for ${rejectedPrompts}`)
+        }
+    })
+
+    app.patch('/select', (req, res) => {
+        if (authenticateRequest(req, res)) {
+            res.send('received request to select new prompt')
+        }
+    })
+
+    app.delete('/', (req, res) => {
+        if (authenticateRequest(req, res)) {
+            const {prompt} = req.body
+            res.send(`deleted prompt ${prompt}`)
+        }
     })
 
     return app
